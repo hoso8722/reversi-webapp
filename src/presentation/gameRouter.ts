9685b3751f537ff92f308bb0fe5e958ebba2e045
jinchangz <jinchangz1@gmail.com>
2024-11-23 15:30:00 +0900

    session ended

--- src/presentation/gameRouter.ts
+++ src/presentation/gameRouter.ts
@@ -1,7 +1,10 @@
-import express from 'express'
-import { StartNewGameUseCase } from '../application/useCase/startNewGameUseCase'
-import { GameMySQLRepository } from '../infrastructure/repository/game/gameMySQLRepository'
-import { TurnMySQLRepository } from '../infrastructure/repository/turn/turnMySQLRepository'
+import express from "express"
+import { StartNewGameUseCase } from "../application/useCase/startNewGameUseCase"
+import { GameMySQLRepository } from "../infrastructure/repository/game/gameMySQLRepository"
+import { TurnMySQLRepository } from "../infrastructure/repository/turn/turnMySQLRepository"
+import { FindLastGamesUseCase } from "../application/useCase/findLastGamesUseCase"
+import { FidnLastGamesMySQLQueryService } from "../infrastructure/query/findLastGamesMySQLQueryService"
+import { WinnerDisc } from "../domain/model/gameResult/winnerDisc"
 
 export const gameRouter = express.Router()
 
@@ -10,7 +13,46 @@ const startNewGameUseCase = new StartNewGameUseCase(
   new TurnMySQLRepository()
 )
 
-gameRouter.post('/api/games', async (req, res) => {
+const findLastGamesUseCase = new FindLastGamesUseCase(
+  new FidnLastGamesMySQLQueryService()
+)
+
+interface GetGamesResponseBody {
+  games: {
+    id: number
+    darkMoveCount: number
+    lightMoveCount: number
+    winnerDisc: number
+    startedAt: Date
+    endAt: Date
+  }[]
+}
+
+gameRouter.get(
+  "/api/games",
+  async (req, res: express.Response<GetGamesResponseBody>) => {
+    const output = await findLastGamesUseCase.run()
+
+    const responseBodyGames = output.map((g) => {
+      return {
+        id: g.gameId,
+        darkMoveCount: g.darkMoveCount,
+        lightMoveCount: g.lightMoveCount,
+        winnerDisc: g.winnerDisc,
+        startedAt: g.startedAt,
+        endAt: g.endAt,
+      }
+    })
+
+    const responseBody = {
+      games: responseBodyGames,
+    }
+
+    res.json(responseBody)
+  }
+)
+
+gameRouter.post("/api/games", async (req, res) => {
   await startNewGameUseCase.run()
 
   res.status(201).end()
